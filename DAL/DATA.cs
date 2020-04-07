68364f03dfe57c2434411b275de6c8bbd8f67e1d
dolemta9899 <61912750+dolemta9899@users.noreply.github.com>
2020-04-07 22:50:53 +0700

    File DAL (hoàn thiện giao diện)
    đây là file hoàn thiện nhé

---
+++ DAL/DATA.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using QLNS.ValueObject;
+
+namespace QLNS.DataAccessLayer
+{
+    public class DAO
+    {
+
+      
+
+        public static int them_TK(TaiKhoan a)
+        {
+            SqlParameter[] para = new SqlParameter[]
+            {
+                new SqlParameter("@acc", a.Acc),
+                new SqlParameter("@pass", a.Pass),
+               
+            };
+            return DataProvider.ExecuteNonQuery("them_TK", para);
+
+        }
+        //phong ban
+
+    }
+}
