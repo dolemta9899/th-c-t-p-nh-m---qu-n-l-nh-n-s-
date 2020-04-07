269b4a46fec1ec4bae0ab5e9cfa6950a4a217c91
dolemta9899 <61912750+dolemta9899@users.noreply.github.com>
2020-04-07 22:53:20 +0700

    file GUI (hoàn chỉnh)
    file này hoàn chỉnh rồi nhé

---
+++ GUI/Main.cs
@@ -0,0 +1,71 @@
+﻿using GUI.ComponentForm;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace GUI
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void panmenu_Paint(object sender, PaintEventArgs e)
+        {
+            //mcáienuStrip1.Hide();
+        }
+
+        private void flat_thoat_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void flat_sach_Click(object sender, EventArgs e)
+        {
+        }
+
+       
+
+     
+
+       
+
+        private void flat_timkiem_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void flat_thongke_Click(object sender, EventArgs e)
+        {
+            
+
+
+        }
+
+        private void fl_help_Click(object sender, EventArgs e)
+        {
+            Process.Start("http://quanlyns.comeze.com/");
+        }
+
+        private void help_Click(object sender, EventArgs e)
+        {
+            Process.Start("http://quanlyns.comeze.com/");
+        }
+
+        private void panmenu_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (e.KeyCode==Keys.H)
+                Process.Start("http://quanlyns.comeze.com/");
+        }
+    }
+}
