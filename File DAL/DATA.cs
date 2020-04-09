b7d65723ccb3d08399b8d32d9d7af1bdd64c46ea
tranhuyen123 <59789766+tranhuyen123@users.noreply.github.com>
2020-04-09 23:15:32 +0700

    Thêm phần sửa tiếp này
    cái này cũng chỉ có code thôi, mình ko up cả project ;))

---
+++ File DAL/DATA.cs
@@ -0,0 +1,34 @@
+﻿           //Phong Ban             
+
+        public static int sua_PB(PhongBan a)
+        {
+            SqlParameter[] para = new SqlParameter[]
+            {
+                new SqlParameter("@mapb", a.MaPB),
+                new SqlParameter("@tenpb", a.TenPB),
+                new SqlParameter("@sdt", a.SDT1),
+                new SqlParameter("@email", a.Email1),
+
+            };
+            return DataProvider.ExecuteNonQuery("sua_PB", para);
+        }
+       
+        //Nhan Vien
+       
+        public static int sua_NV(NhanVien a)
+        {
+            SqlParameter[] para = new SqlParameter[]
+            {
+                new SqlParameter("@manv", a.MaNV),
+                new SqlParameter("@tennv", a.TenNV),
+                new SqlParameter("@ngaysinh", a.NgaySinh),
+                new SqlParameter("@gioitinh", a.GioiTinh),
+                new SqlParameter("@diachi", a.DiaChi),
+                  new SqlParameter("@sdt", a.SDT1),
+                    new SqlParameter("@mapb", a.MaPB),
+                      new SqlParameter("@luong", a.Luong1),
+
+            };
+            return DataProvider.ExecuteNonQuery("sua_NV", para);
+        }
+       
